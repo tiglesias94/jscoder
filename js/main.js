48f16b509107e7a3f05b10114c4c9b26b439ec33
tiglesias <iglesias.t94@gmail.com>
2022-07-19 23:30:17 -0300

    Calculadora de costos de envio

--- js/main.js
+++ js/main.js
@@ -1,37 +1,3 @@
-//Usuarios y contraseñas registradas como proveedores
-let saleNames = ['User1','User2', 'User3', 'User4'];
-let salePass = ['pass1', 'pass2', 'pass3', 'pass4'];
-
-//Usuarios y contraseñas registradas como vendedores
-let buyerNames = ['User5','User6', 'User7', 'User8'];
-let buyerPass = ['pass5', 'pass6', 'pass7', 'pass8'];
-
-
-function login(params) {
-    let userLog = document.getElementsByClassName("userLogin"),
-            loginInput = [];         
-        for (var i = 0; i < userLog.length; i+=1) {    
-            loginInput[i] = userLog[i].value;    
-            }  
-        let userName = loginInput[0]
-        let password = loginInput [1]
-        loginCheck(userName, password)
-}
-
-function loginCheck(userName, password) {
-
-    if (saleNames.includes(userName) && salePass.includes(password)) {
-        window.open("https://tiglesias94.github.io/jscoder/pages/admin.html")
-    } else 
-        {if (buyerNames.includes(userName) && buyerPass.includes(password)) {
-            window.open("https://tiglesias94.github.io/jscoder/pages/user.html")
-            } else {
-            alert("Usuario / Contraseña Incorrectos")
-            }
-        }
-}
-
-
 let myLatLng = { lat: 38.3460, lng: -0.4907 }; //Objeto --> parametros para definir punto cero del mapa
 let mapOptions = { // Objeto --> Propiedades del mapa 
     center: myLatLng,
@@ -62,14 +28,19 @@ function calcRoute() {
     }
 
     //pass the request to the route method
-    directionsService.route(request, function (result, status) {
+    directionsService.route(request, 
+        function (result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
 
             //Get distance and time
             const output = document.querySelector('#output');
-            output.innerHTML = "<h3> Origen: " + document.getElementById("from").value + "</h3>" + ".<h3> Destino: " + document.getElementById("to").value + "</h3>" +"<h4> Distancia de Encomienda: " + result.routes[0].legs[0].distance.text + "</h4>";
+            output.innerHTML = 
+            
+
             //display route
-            directionsDisplay.setDirections(result);
+            directionsDisplay.setDirections(result)
+
+            
         } else {
             //delete route from map
             directionsDisplay.setDirections({ routes: [] });
@@ -77,10 +48,73 @@ function calcRoute() {
             map.setCenter(myLatLng);
 
             //show error message
-            output.innerHTML = "<div> <p> No fue posible encontrar una ruta valida para la encomienda </p> </div>";
+            output.innerHTML = "<div> <p> No fue posible encontrar una ruta valida para la encomienda </p> </div>";   
         }
-    });
+    
 
+        //Calculo costo de envio
+        
+        const priceFactor = {
+            pricekm:10,
+            areaFactor:0.15,
+            volumeFactor:0.010,
+            weightFactor:50
+        }
+        
+        
+        const distance = parseInt(result.routes[0].legs[0].distance.text)
+        const weight = document.getElementById("weight").value
+        const volume = document.getElementById("sizeH").value*document.getElementById("sizeW").value*document.getElementById("sizeD").value
+        const area = document.getElementById("sizeW").value*document.getElementById("sizeD").value
+        
+        function fragileObj() {
+            const fragile = document.getElementById("fragile").value
+            if (fragile === "true") {
+                fragileFactor = 1.1
+                console.log ("El objeto es FRAGIL")
+            } else { 
+                fragileFactor = 1
+                console.log ("El objeto NO es FRAGIL")    
+            }
+            return (fragileFactor)
+        }
+
+        function stackObj() {
+            const stack = document.getElementById("stack").value
+            if (stack === "true") {
+                stackFactor = 1
+                console.log ("El objeto es APILABLE")
+            } else { 
+                stackFactor=1.1
+                console.log ("El objeto NO es APILABLE")    
+            }
+            return (stackFactor)
+        }
+        
+        fragileObj()
+        stackObj()
+   
+        console.log("Distancia de envio: " + distance)
+        console.log("Area del paquete: " + area)
+        console.log("Volumen del paquete: " + volume)
+        console.log("Peso del paquete: " + weight)
+               
+        let price = parseInt(stackFactor * fragileFactor * (priceFactor.pricekm*distance) + (priceFactor.areaFactor * area) + (priceFactor.volumeFactor*volume) + (priceFactor.weightFactor*weight));
+        
+
+        console.log("-----------------------")
+        console.log("Precio calculado del viaje: " + price)     
+        
+        const output = document.querySelector('#calculations');
+        output.innerHTML = 
+        "<h2> Detalles de tu encomienda:  </h2>"+
+        "<h3> Paquete: " + document.getElementById("object").value +
+        "<h3> Origen: " + document.getElementById("from").value + "</h3>" + 
+        "<h3> Destino: " + document.getElementById("to").value + "</h3>" +
+        "<h3> Distancia de Encomienda: " + result.routes[0].legs[0].distance.text + "</h4>" +
+        "<h3> Precio de Encomienda: $ " + price + "</h3>";
+    })
+    ;
 }
 
 //create autocomplete objects for all inputs
@@ -93,3 +127,4 @@ var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
 
 var input2 = document.getElementById("to");
 var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
+
